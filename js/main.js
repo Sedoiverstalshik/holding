1f4e627169be5c9b82cf549ae27b66965e8c4b59
Sedoiverstalshik <104726458+Sedoiverstalshik@users.noreply.github.com>
2022-12-25 00:09:32 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-const swiper = new Swiper('.swiper', {
+const projectsSwiper = new Swiper('.projects__swiper', {
   loop: true,
   slidesPerView: 3,
   spaceBetween: 40,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.projects__button-next',
+    prevEl: '.projects__button-prev',
+  },
+});
+const projectsSliderSwiper = new Swiper('.projects-slider__swiper', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 100,
   centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
