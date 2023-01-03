7882dc8a7eb89a2f52809bd25314973e1089c64a
Sedoiverstalshik <104726458+Sedoiverstalshik@users.noreply.github.com>
2023-01-04 00:09:54 +0300

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,30 @@
+<?php
+
+/* https://api.telegram.org/bot5975232400:AAEiE4qORD9V3voDHWvvHoY1VI6yi-RNOSQ/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$token = "5975232400:AAEiE4qORD9V3voDHWvvHoY1VI6yi-RNOSQ";
+$chat_id = "-824868082";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+  'Сообщение' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
